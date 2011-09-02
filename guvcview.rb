124ad0659a18eb93857035145c04dcf2afee9d9d
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-09-02 00:36:55 -0700

    quit when done

--- guvcview.rb
+++ guvcview.rb
@@ -7,4 +7,5 @@ command << " -d #{buddy.video_device[:file_system_device]}"
 command << " -g /usr/local/icl_capture/guvcsettings"
 command << " -n #{buddy.file_system[:path]}/#{buddy.capture_name}"
 command << " -t 25200"
+command << " --exit_on_close"
 puts command
