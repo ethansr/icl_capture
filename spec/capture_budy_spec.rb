7184678209adbd54b3d2d88fdc6c50b8147a0f9e
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-09-01 23:07:03 -0700

    failing specs for default camera buddy behavior

--- spec/capture_budy_spec.rb
+++ spec/capture_budy_spec.rb
@@ -2,5 +2,15 @@ require 'lib/capture_buddy'
 
 describe CaptureBuddy do
 
-
+  describe "create default camera buddy" do
+    before :each do 
+      @buddy = CaptureBuddy.new
+    end
+    it "should find a UVC camera" do
+      @buddy.video_device[:label].should match /UVC/
+    end
+    it "should find a USB audio device" do
+      @budy.audio_device[:label].should match /USB/
+    end
+  end
 end
