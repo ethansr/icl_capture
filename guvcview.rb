e9d5982143c8355478a1a2f378831b9f72fbe60a
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-09-02 01:43:15 -0700

    tweak

--- guvcview.rb
+++ guvcview.rb
@@ -4,7 +4,7 @@ buddy = CaptureBuddy.new
 command = ""
 command << "DISPLAY=:0 guvcview"
 command << " -d #{buddy.video_device[:file_system_device]}"
-command << " -g /usr/local/icl_capture/guvcsettings"
+#command << " -g /usr/local/icl_capture/guvcsettings"
 command << " -n #{buddy.file_system[:path]}/#{buddy.capture_name}"
 command << " -t 25200"
 command << " --exit_on_close"
