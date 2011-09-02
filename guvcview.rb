6dde77d73fb493d0b8e346bc9803e05b2ec61dd4
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-09-02 00:33:25 -0700

    add bit about display

--- guvcview.rb
+++ guvcview.rb
@@ -2,7 +2,7 @@ require 'lib/capture_buddy'
 
 buddy = CaptureBuddy.new
 command = ""
-command << "guvcview"
+command << "DISPLAY=:0 guvcview"
 command << " -d #{buddy.video_device[:file_system_device]}"
 command << " -g /usr/local/icl_capture/guvcsettings"
 command << " -n #{buddy.file_system[:path]}/#{buddy.capture_name}"
