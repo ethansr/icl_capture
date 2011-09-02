6a0082c3127ff4203f4ad660c365bf5812359f2b
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-09-02 00:18:39 -0700

    too tired

--- guvcview.rb
+++ guvcview.rb
@@ -1,7 +1,7 @@
 require 'lib/capture_buddy'
 
 buddy = CaptureBuddy.new
-
+command = ""
 command << "guvcview"
 command << "-d #{buddy.video_device[:label]}"
 command << "-g /usr/local/icl_capture/guvcsettings"
