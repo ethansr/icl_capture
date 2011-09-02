b019300579689792c0e458a300a2ee2b7eda6a38
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-09-02 00:23:59 -0700

    types

--- guvcview.rb
+++ guvcview.rb
@@ -3,8 +3,8 @@ require 'lib/capture_buddy'
 buddy = CaptureBuddy.new
 command = ""
 command << "guvcview"
-command << "-d #{buddy.video_device[:label]}"
-command << "-g /usr/local/icl_capture/guvcsettings"
-command << "-n #{buddy.file_system[:path]}/#{buddy.capture_name}"
-command << "-t 25200"
-command
+command << " -d #{buddy.video_device[:file_system_device]}"
+command << " -g /usr/local/icl_capture/guvcsettings"
+command << " -n #{buddy.file_system[:path]}/#{buddy.capture_name}"
+command << " -t 25200"
+puts command
