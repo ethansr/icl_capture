50cd6730f50a69bec2e5233fe40b8333f526fdbd
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-09-03 02:15:52 -0700

    mjpeg code is mjpg, this i sthe default anyway dunno if this matters

--- guvcview.rb
+++ guvcview.rb
@@ -9,7 +9,7 @@ command << " -d #{buddy.video_device[:file_system_device]}"
 #command << " -g /usr/local/icl_capture/guvcsettings"
 command << " -n #{buddy.file_system[:path]}/#{buddy.capture_name}.avi"
 command << " -s 1280x720"
-command << " -f mjpeg"
+command << " -f mjpg"
 command << " -t 25200"
 command << " --exit_on_close"
 puts command
